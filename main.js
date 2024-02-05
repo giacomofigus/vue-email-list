ace4057a1f46ae58ee2169a5391c88de9e190f86
MyUsername <figusgiacomo04@gmail.com>
2024-02-05 16:00:06 +0100

    to-fix? : js-for emails

--- main.js
+++ main.js
@@ -12,10 +12,12 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-
+            email: "",
         }
     },
     mounted(){
-        
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) =>{
+            this.email = result.data.response
+        })
     }
 }).mount("#app")
